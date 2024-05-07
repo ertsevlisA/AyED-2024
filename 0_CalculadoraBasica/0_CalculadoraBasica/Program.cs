673c4e255c7047f2e25ee009d5a2704222d59708
ertsevlisA <167466523+ertsevlisA@users.noreply.github.com>
2024-05-07 16:00:11 -0300

    Add files via upload

---
+++ 0_CalculadoraBasica/0_CalculadoraBasica/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_CalculadoraBasica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            Console.WriteLine("decime un numero cualquiera");
+            num1=int.Parse(Console.ReadLine());
+            Console.WriteLine("ahora decime otro mas");
+            num2=int.Parse(Console.ReadLine());
+            Console.WriteLine("la suma de estos dos numeros es" +( num1 + num2));
+            Console.WriteLine("la resta de estos dos numeros es" + (num1 - num2));
+            Console.WriteLine("y la multi de estos dos numeros es" + (num1 * num2));
+            Console.WriteLine("su division de estos dos numeros es" + (num1 / num2));
+            Console.WriteLine("y su resto es" + (num1 % num2));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 3_DimesionDeEsfera/3_DimensionDeEsfera/3_DimensionDeEsfera/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_DimensionDeEsfera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int radio;
+            Console.WriteLine("Decime el radio de la esfera");
+            radio = int.Parse(Console.ReadLine());
+            Console.WriteLine("la superficie es de=" + (4*Math.PI*(radio * radio)));
+            Console.WriteLine("el volumen es de=" + (4 * Math.PI / 3*(radio * radio * radio)));
+            Console.ReadKey();
+        }
+    }
+}
