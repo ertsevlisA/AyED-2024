eb22ceec770131245ddbb5880ad500583c558b8b
ertsevlisA <167466523+ertsevlisA@users.noreply.github.com>
2024-05-20 20:06:27 -0300

    Add files via upload

---
+++ Etapa1/8_EsMayor/8_EsMayor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_EsMayor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad;
+            Console.WriteLine("¿cual es tu edad?");
+            edad = int.Parse(Console.ReadLine());
+            if (edad >= 18)
+            {
+                Console.WriteLine("sos mayor de edad");
+            }
+            else if (edad <= 18)
+            {
+                Console.WriteLine("no sos mayor de edad");
+                Console.ReadKey();
+            }
+        }
+
+    }
+}
---
+++ Etapa1/9_IngresePassword/9_IngresePassword/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_IngresePassword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contraseña;
+            Console.WriteLine("¿cual es la contraseña?");
+            contraseña = int.Parse(Console.ReadLine());
+        }
+    }
+}
