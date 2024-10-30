556f39d8c7d00c7796cc40873a6de483008f81ff
ertsevlisA <167466523+ertsevlisA@users.noreply.github.com>
2024-10-30 16:08:10 -0300

    Add files via upload

---
+++ etapa3/0_MiPrimerFuncion/0_MiPrimerFuncion/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_MiPrimerFuncion
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //Escribir una función a la que se le pase una cadena<nombre>
+            //y muestre por pantalla el saludo ¡hola < nombre > !
+            
+            string nombre = "aylu";
+            saludar(nombre);
+        }
+        static string saludar (string nombre)
+        {
+
+
+
+            string saludo=("hola" + nombre);
+            return saludo;
+                
+         }
+      
+        
+    }
+}
+
---
+++ etapa3/1_SistemaDeConversiones/1_SistemaDeConversiones/SistemaDeConversiones(1).cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_SistemaDeConversiones
+{
+    class Program
+    { static void Main(string[] args)
+        {
+            int num = 10;
+            int tamaño = 4;
+            int[] numero = { 0, 1, 0, 0 };
+            Console.WriteLine("numero " + num + " en binario: ");
+            Console.Write((numeroBinario(num)));
+            Console.WriteLine(" numero " + string.Join("", numero) + " en decimal: " + (numeroDecimal(0, tamaño, numero)));
+
+            Console.ReadKey();
+        }
+        static int numeroBinario(int numero)
+        {
+            if (numero > 0)
+            {
+                String lista = "";
+                while (numero > 0)
+                {
+                    if (numero % 2 == 0)
+                    {
+                        lista = "0" + lista;
+                    }
+                    else
+                    {
+                        lista = "1" + lista;
+                    }
+                    numero = (int)(numero / 2);
+                }
+                Console.WriteLine(lista);
+            }
+            return numero;
+        }
+        static int numeroDecimal(int puntos, int tam, int[] losNumeros)
+        {
+            for (int i = 0; i < tam; i++)
+            {
+                puntos += losNumeros[i] * (int)Math.Pow(4, 1);
+            }
+
+            return puntos;
+        }
+    }
+}
+
