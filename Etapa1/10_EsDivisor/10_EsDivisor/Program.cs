f17c2070c1031af204bfa81259e8ecd8efc2b3e8
ertsevlisA <167466523+ertsevlisA@users.noreply.github.com>
2024-10-30 16:48:37 -0300

    Add files via upload

---
+++ Etapa1/10_EsDivisor/10_EsDivisor/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_EsDivisor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+
+            Console.WriteLine("Ingrese un numero");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese otro numero");
+            num2 = int.Parse(Console.ReadLine());
+
+            if (num2 == 0)
+            {
+                Console.WriteLine("Error");
+            }
+            else
+            {
+                Console.WriteLine("El resultado es " + (num1 / num2));
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
