a07bc3dc10508c80150c0dc8fff47e46ffa76d11
ertsevlisA <167466523+ertsevlisA@users.noreply.github.com>
2024-10-30 16:25:08 -0300

    Add files via upload

---
+++ etapa4/2 - LoginSimple/2 - LoginSimple/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2___LoginSimple
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string usuario = "admin";
+            string contranseña = "admin12345";
+
+            textBox2.PasswordChar = '*';
+
+
+            if (textBox1.Text==usuario & textBox2.Text== contranseña)
+            {
+                MessageBox.Show("Autorizado a ingresar");
+            }
+            else
+            {
+                MessageBox.Show("Usuario o contraseña incorrecta");
+            }
+        }
+    }
+}
---
+++ etapa4/3 - TerminosYCondiciones/3 - TerminosYCondiciones/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3___TerminosYCondiciones
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = checkBox1.Checked;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                
+                Application.Exit(); 
+            }
+        }
+    }
+}
