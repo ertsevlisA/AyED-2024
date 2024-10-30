fb225447da72dccaa14eff2c0766d300e7a86ab5
ertsevlisA <167466523+ertsevlisA@users.noreply.github.com>
2024-10-30 16:17:04 -0300

    Add files via upload

---
+++ etapa0/2_TestVariable/2_TestVariable/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_TestVariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int entero;
+            bool boolean;
+            float flotante;
+            Double doble;
+            Char caracter;
+            String cadena;
+
+            caracter = '1';
+            cadena = "15.55794";
+            entero = 15;
+            boolean = false;
+            flotante = 15.557294f;
+            doble = 15.557294;
+
+            Console.WriteLine("String: " + cadena);
+            Console.WriteLine("int: " + entero);
+            Console.WriteLine("bool: " + boolean);
+            Console.WriteLine("float: " + flotante);
+            Console.WriteLine("Char: " + caracter);
+            Console.WriteLine("Double: " + doble);
+            entero = int.Parse(Console.ReadLine());//NO PONER LETRA
+            Console.ReadKey();
+        }
+    }
+}
